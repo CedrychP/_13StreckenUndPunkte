80fd5e75ce3e1c294110d23da258e10ebce94a2d
Philipp Cedrych <philipp.cedrych@compax.at>
2023-11-09 15:46:55 +0100

    Initial commit

---
+++ src/StreckenTest.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+  public static Strecke strecken[] = {
+      new Strecke(new Punkt(3, 5), new Punkt(7, 6)),
+      new Strecke(new Punkt(3, 6), new Punkt(8, 7)),
+      new Strecke(new Punkt(4, 7), new Punkt(9, 7)),
+      new Strecke(new Punkt(10, 11), new Punkt(12, 13))
+  };//
+}
